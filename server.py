06d175105acbd1a91a1fd333352e43ab1433caa8
carlos.pasquali.dev <carlos.pasquali.dev@gmail.com>
2023-12-05 19:29:38 -0300

    add documentation

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 from src.api.routes import routerBlueprint
+
 
 # INIT FLASK
 app = Flask(__name__)
 
 # REGISTER ROUTER BLUEPRINT
 app.register_blueprint(routerBlueprint, url_prefix='/api')
+
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
